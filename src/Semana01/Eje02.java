5a387970a6a225250c4e939e142417c8c7de49b8
Christian Garcia <ubuntox84@gmail.com>
2022-10-04 10:22:00 -0500

    octubre

---
+++ src/Semana01/Eje02.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana01;
+
+/**
+ *
+ * @author chris
+ */
+public class Eje02 {
+    public static void main(String[] args) {
+        System.out.println("Hasta el agujero negro ");
+        System.out.println("");
+    }
+}
---
+++ src/Semana01/Ejer01.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Semana01;
+
+/**
+ *
+ * @author chris
+ */
+public class Ejer01 {
+    // psvm   --> TAB
+    public static void main(String[] args) {
+        // sout   ---> TAB
+        System.out.print("FIIS - UNAS\n\n\n");
+        //Guardar el archivo --> ctrl + s
+        // para el signo "\" --> alt + 92
+    }
+}
